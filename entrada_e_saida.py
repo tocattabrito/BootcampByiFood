607dea3ca06cfb5ace2aac4268fba6c4e0f2497c
tocattabrito <tocattabrito@gmail.com>
2023-07-02 10:24:19 -0300

    Remoção dos arquivos duplicados

--- entrada_e_saida.py
+++
@@ -1,8 +0,0 @@
-nome = input("Informe seu nome: ")
-idade = input("Informe sua idade: ")
-
-
-print(nome , idade)
-print(nome , idade, end="...\n")
-print(nome , idade, sep="#", end="...\n")
-print(nome , idade, sep="#")
