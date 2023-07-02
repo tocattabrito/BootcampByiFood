c0fdb41788c9dfd10196261fe2dcad92fc211bd8
tocattabrito <tocattabrito@gmail.com>
2023-07-01 23:35:58 -0300

    Aula 3 de Python

---
+++ variaveis_constantes.py
@@ -0,0 +1,17 @@
+#  variáveis
+idade = 28
+nome = 'Guilherme'
+print(f'Meu nome é {nome} e tenho {idade} ano(s) de idade.')
+
+
+idade = 27
+nome = 'Giovanna'
+print(f'Meu nome é {nome} e tenho {idade} ano(s) de idade.')
+
+
+limite_saque_diario = 1000
+
+
+# Constante
+ESTADOS = ['PE','AL','MG','SC','PB',]
+print(ESTADOS)
