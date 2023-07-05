8d6ec346454a9fb2dd9cb4d1e10b9be6fef6e89a
tocattabrito <tocattabrito@gmail.com>
2023-07-04 23:22:30 -0300

    Manipulando Strings com Python

---
+++ python/string_2.py
@@ -0,0 +1,23 @@
+nome = "Guilherme"
+idade = 28
+profissão = "Programador"
+linguagem = "Python"
+
+saldo = 45.435
+
+dados = {"nome": "Guilherme", "idade": 28} # dicionário
+
+print("Nome: %s Idade: %d" % (nome, idade))
+
+print("Nome: {} Idade: {}".format(nome, idade))
+
+print("Nome: {1} Idade: {0}".format(idade, nome)) # positivo
+print("Nome: {1} Idade: {0} Nome: {1} {1}".format(idade, nome))
+
+print("Nome: {nome} Idade: {idade}".format(nome=nome, idade=idade))
+print("Nome: {name} Idade: {age} {name} {name} {age}".format(age=idade, name=nome))
+print("Nome: {nome} Idade: {idade}".format(**dados)) # uso do dicionário
+
+print(f"Nome: {nome} Idade: {idade}")
+print(f"Nome: {nome} Idade: {idade} Saldo: {saldo:.2f}")
+print(f"Nome: {nome} Idade: {idade} Saldo: {saldo:10.1f}")
---
+++ python/string_4.py
@@ -0,0 +1,27 @@
+nome = "Guilherme"
+
+mensagem = f'''
+	Olá meu nome é {nome},
+Eu estou aprendendo Python.
+		Essa mensagem tem diferentes recuos.
+'''
+print(mensagem)
+
+
+print(
+    """
+    ============= MENU =============
+
+    1 - Depositar
+    2 - Sacar
+    0 - Sair
+
+    ================================
+
+            Obrigado por usar nosso sistema!!!!
+"""
+)
+
+                                                         
+curso = "Python"                                                     
+print(curso[::-1])   
