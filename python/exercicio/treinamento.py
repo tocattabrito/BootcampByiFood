b39e0f1cdc156c7f5cbd7a82fcd72d0236391da9
tocattabrito <tocattabrito@gmail.com>
2023-07-10 12:57:44 -0300

    Adicionado mais opções no menu e seus métodos

--- python/exercicio/treinamento.py
+++ python/exercicio/treinamento.py
@@ -1,17 +1,21 @@
 # import
 import os
 
+# constantes
+LIMITE_SAQUE = 3
+AGENCIA = "0001"
 
-# Variáveis
+
+# variáveis
 saldo = 0
 limite = 500
 extrato = ""
 valor = 0
-# valor_depositado = 0
 numero_saque = 0
-LIMITE_SAQUE = 3
+usuarios = []
+contas = []
 
-
+# MÉTODOS:
 
 # menu
 def menu():
@@ -20,37 +24,59 @@ def menu():
 
 Menu
           
-[d] Depósito
-[s] Saque
-[e] Extrato
+[d] Depósito    [nc] Nova Conta
+[s] Saque       [lc] Listar Conta
+[e] Extrato     [nu] Novo Usuário
 [q] Sair
           
                               v.02          
 =======================================   
 """)
-
-# Telas     
-def tela_deposito():
-    print("\n ============= DEPÓSITO ==============\n")
-
-def tela_saque():
-    print("\n ============ SAQUE ================\n")
-
-# def tela_extrato():
-#     print("\n ============ DEPOSITO ================\n")
-
-
-
-   
-
           
- 
+# criar usuário
+def criar_usuario(usuarios):
+    cpf = input("Informe o CPF (somente número): ")
+    usuario = filtrar_usuario(cpf, usuarios)
 
-# Funções:
+    if usuario:
+        print("Já existe usuário com esse CPF!")
+        return
 
-# limpar tela
-def limpar_tela():
-    os.system('clear')
+    nome = input("Informe o nome completo: ")
+    data_nascimento = input("Informe a data nascimento (dd-mm-aaaa): ")
+    endereco = input("informe o endereço (logradouro, nro - bairro - cidade/sigla estado): ")
+
+    usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco})
+
+    print("=== Usuário criado com sucesso! ===")
+
+# criar conta
+def criar_conta(agencia, numero_conta, usuarios):
+    cpf = input("Informe o CPF do usuário: ")
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print("Conta criada com sucesso!")
+        return {"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario}
+
+    print("Usuário não encontrado, fluxo de criação de conta encerrado!")
+
+# listar contas
+def listar_contas(contas):
+    for conta in contas:
+        linha = f"""\
+            Agência:{conta['agencia']}
+            C/C:{conta['numero_conta']}
+            Titular:{conta['usuario']['nome']}
+        """
+        print("=" * 100)
+    #   print(textwrap.dedent(linha))
+
+
+# Pesquisar usuário
+def filtrar_usuario(cpf, usuarios):
+    usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return usuarios_filtrados[0] if usuarios_filtrados else None
 
 # depositar
 def depositar(saldo, valor, extrato, /):
@@ -102,9 +128,10 @@ def sacar():
 def exibir_extrato():
     print("\n===============EXTRATO==================")
     print("Não foram realizadas movimentações." if not extrato else extrato)
-    print(f"\nSaldo: R$ {saldo:.2f}")
+    print(f"""\nSaldo:  R$ {saldo:.2f}""")
     print("========================================")
 
+# MENSAGEM
 # Saldação de encerramento do programa
 def mensagem_final():
     print("""
@@ -115,28 +142,35 @@ tenha um excepcional dia.
           
     """)
 
-# Chamar os métodos
-# menu()
+# TELAS 
 
-# saldo, extrato = depositar(saldo, valor, extrato)
-# sacar()
-# exibir_extrato()
-# mensagem_final()
+def limpar_tela():
+    os.system('clear')
 
+def tela_deposito():
+    print("\n ============= DEPÓSITO ==============\n")
+
+def tela_saque():
+    print("\n ============== SAQUE ================\n")
+
+
+
+# Funcionalidades do menu
 while True:
-    # codição de parada do laço while que verifica a opção escolhida
-    # teste = (input("Uma opção: "))
+   
     menu() # Iniciação do programa
     opcao = input("Digite uma opção: \n==> ")
     limpar_tela()
+   
+    # Chama o método depositar()
     if opcao == "d":
         tela_deposito()
-        saldo, extrato = depositar(saldo, valor, extrato)
-        
+        saldo, extrato = depositar(saldo, valor, extrato)     
         voltar = input("""
 =====================================
                        
 Deseja realizar outra operação? [s/n]\n==> """)
+        # Verifica se o usuário deseja realizar outra operação
         if voltar == "s":
             limpar_tela()
         elif voltar == "n":
@@ -144,17 +178,33 @@ Deseja realizar outra operação? [s/n]\n==> """)
             limpar_tela()
             mensagem_final()
             break
-            
+   
+    # chama o método sacar
     elif opcao == "s":
         tela_saque()
         sacar()
         limpar_tela()
-        
+    
+    # chama o método  exibir_extrado    
     elif  opcao == "e":
         exibir_extrato()
-        
 
- # comando para encerrar a aplicação 
+    # chama o método criar conta
+    elif opcao == "nc":
+        numero_conta = len(contas) + 1
+        conta = criar_conta(AGENCIA, numero_conta, usuarios)
+
+        if conta:
+            contas.append(contas)
+
+    # chama o método que cria um novo usuário
+    elif opcao == "nu":
+        criar_usuario(usuarios)
+
+    elif opcao == "lc":
+        listar_contas(contas) # ---->  ATENÇÃO <---- buscar a causa do erro
+        
+    # comando para encerrar a aplicação 
     elif opcao == "q":
         mensagem_final()
         break
